af739cb9b790c2822a488b601f8ce75be76ad82a
Alex <alex.r.otero12@gmail.com>
2019-07-16 18:45:13 -0500

    server runs static html file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express()
-const PORT = 3000
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('got index.html');
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
+
+
 
 
 
