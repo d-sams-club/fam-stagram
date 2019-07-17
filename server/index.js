b5fce92561f90c8831ab2ec9181fdeb46720d4f7
Semaj <semajbarthelemy55@gmail.com>
2019-07-16 19:27:10 -0500

    added static middle ware; chnaged sendFile to render

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const app = express();
-const PORT = 3000
+const app = express()
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname+ '/../client/'))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
 
 
 
 app.listen(PORT, () => {
-  console.log(`app listening on ${PORT}!`)});
+  console.log(`app listening on ${PORT}!`)
+});
