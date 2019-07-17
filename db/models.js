70b4c3637f5ce892624a189b2088fc9c74abf039
Darrian Brown <Darrian Brown>
2019-07-17 17:53:13 -0500

    added timestamps

--- db/models.js
+++ db/models.js
@@ -4,18 +4,23 @@ const db = new Sequelize('famstagram', 'root', '', {
   dialect: 'mysql'
 })
 
+
 const User = db.define('user', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: Sequelize.STRING },
   email: { type: Sequelize.STRING, validate: {isEmail:true} },
-  password: { type: Sequelize.STRING }, 
+  password: { type: Sequelize.STRING },
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
 });
  
 
  const Family = db.define('family', {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: Sequelize.STRING },
-    code: { type: Sequelize.STRING, unqiue: true}
+    code: { type: Sequelize.STRING, unqiue: true},
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
 });
 
   const Message = db.define('message', {
@@ -23,6 +28,8 @@ const User = db.define('user', {
     text: { type: Sequelize.STRING },
     userId: {type: Sequelize.INTEGER, references: { model: 'users', key: 'id'} },
     familyId: {type: Sequelize.INTEGER, references: { model: 'families', key: 'id'} },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
   });
 
   const Photo = db.define('photo', {
@@ -30,19 +37,25 @@ const User = db.define('user', {
     text: { type: Sequelize.STRING },
     url: { type: Sequelize.STRING },
     caption: { type: Sequelize.STRING },
-    userId: {type: Sequelize.INTEGER, references: { model: 'users', key: 'id'} }
+    userId: {type: Sequelize.INTEGER, references: { model: 'users', key: 'id'} },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
   });
 
   const Reaction = db.define('reaction', {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     text: { type: Sequelize.STRING },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
   });
 
   const ReactionPhoto = db.define('reactionPhoto', {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     text: { type: Sequelize.STRING },
     familyId: {type: Sequelize.INTEGER, references: { model: 'families', key: 'id'} },
-    userId: {type: Sequelize.INTEGER, references: { model: 'users', key: 'id'} }
+    userId: {type: Sequelize.INTEGER, references: { model: 'users', key: 'id'} },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
   });
 
 module.exports = db;
