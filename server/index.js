436dddfa6661c8391944be12349ae78e95e096e5
Alex <alex.r.otero12@gmail.com>
2019-07-16 19:23:50 -0500

    merging upstream

--- server/index.js
+++ server/index.js
@@ -3,9 +3,19 @@ const app = express()
 const path = require('path');
 const bodyParser = require('body-parser');
 const PORT = 3000;
+// needs to handle routes for all pages\\
+// routes: sign-log, homepage, groups, chats
 
+//signup/login page
+/*data handlers: /sign-log/:newUser (post) post request bc we are putting a new user on the user table in the db
+                /sign-log/:user (get) <<--- maybe get request because we already have that user on the db
+                /
+
+*/
+// probably wont use just /
+app.use('/static', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
   console.log('got index.html');
