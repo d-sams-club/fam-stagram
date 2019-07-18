0422fefb2342d3bcafea0fefff90aa296c668442
Alex <alex.r.otero12@gmail.com>
2019-07-18 12:57:00 -0500

    serializing the user into sesh

--- server/index.js
+++ server/index.js
@@ -1,20 +1,86 @@
+// REQUIRED STUFF
 const express = require('express');
-const app = express()
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 const db = require('../db/index');
+const userInViews = require('./middleware/userInViews');
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+
+
+const app = express()
+dotenv.config();
 const PORT = 3000;
+
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+///////////////////////////////////////////////////////////////////
+//  MIDDLEWARE AND AUTH
+///////////////////////////////////////////////////////////////////
+/*
+// secret: JACK nut VISA jack music TOKYO 5 APPLE MUSIC BESTBUY VISA xbox 6 7 3 7 6 visa 3 COFFEE ROPE BESTBUY queen apple nut TOKYO hulu skype KOREAN 
+// 7 queen XBOX tokyo TOKYO hulu music bestbuy bestbuy golf ROPE XBOX ROPE korean LAPTOP golf USA apple usa
+*/
+const sess = {
+  secret: 'JnVjmT5AMBVx67376v3CRBqanThsK7qXtThmbbgRXRkLgUau',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+app.use(session(sess));
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+  passport.use(strategy);
+      passport.serializeUser(function(user, done) {
+        console.log(user);
+        done(null, user.id);
+      });
+      passport.deserializeUser(function(user, done) {
+        done(null, user);
+      });
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static(__dirname+ '/../client/'))
 app.use(bodyParser.json());
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
 
-// will handle all authenticating in server
-// domain: dev-sn985q6v.auth0.com
-// client id: EhZnOVZG6gBJZvh0tRLL5mSJVUAMFMb2
-// client secret: my7ovaY3uSVSYDrE4K0VRJanVqf208kP356mxy5qSQT2BORh
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+
+
 
 
 ///////////////////////////////////////////////////////////////////
@@ -23,13 +89,15 @@ app.use(bodyParser.json());
 
 // the home page with the join family, create family, and logout
 app.get('/', (req, res) => {
+  console.log('page loaded');
   res.render('index');
 });
 
 // the 'auth' page, where the login and signup will be
-app.get('/signLog', (req, res) => {
+app.get('/login', (req, res) => {
   // once front end people give me a file for the signin/signup page i will be able to render it
   // --> res.render('templates/signLog')
+  res.render('index');
 });
 
 // family page where the photos will be displayed fist, with the nav bar with: share code, leave group, chats, gallery(wont do anything ) !! might just let gallery be
---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var secured = require('../middleware/secured');
+var router = express.Router();
+
+router.get('/user', secured(), function (req, res, next) {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render('user', {
+    userProfile: JSON.stringify(userProfile, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
