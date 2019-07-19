44d8776594abeadb25f6c21bfb8472c728438635
Semaj <semajbarthelemy55@gmail.com>
2019-07-19 13:48:27 -0500

    added route for pictures, get and post

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const multer =require('multer');
 const express = require('express');
 const app = express()
 const path = require('path');
 const bodyParser = require('body-parser');
+const fs = require('fs'); 
+const http = require('http');
 const PORT = 3000;
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -10,8 +14,50 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname+ '/../client/'))
 app.use(bodyParser.json());
 
+const upload = multer({
+  dest: __dirname + "/pictures/raw"
+});
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/pictures', upload.single("file"),(req, res) => {
+  console.log(req);
+  const tempPath = req.file.path;
+  const targetPath = path.join(__dirname, `/pictures/${req.file.originalname}`);
+
+  if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+    fs.rename(tempPath, targetPath, err => {
+      if (err){ 
+        console.log(err);
+        return handleError(err, res);
+      }
+
+      fs.appendFile(`${__dirname}/pictures/order.txt`, `${req.file.originalname}\n`)
+      res.status(200)
+        .contentType("text/plain")
+        .end("File uploaded!");
+    });
+  } else {
+    fs.unlink(tempPath, err => {
+      if (err) {
+        console.log(err);
+        return handleError(err, res);
+      }
+
+      res.status(403)
+        .contentType("text/plain")
+        .end("Only .png files are allowed!");
+    });
+  }
 });
 
 
