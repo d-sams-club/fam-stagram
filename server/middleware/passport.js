6b2f96b247c6a64f52b855d7eafe1ca98c607be8
Darrian Brown <Darrian Brown>
2019-07-21 14:10:58 -0500

    Updated passport to support new authO variables and callback url Added mocha test

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -9,7 +9,7 @@ const strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-    'http://localhost:3000/callback' || `http://${process.env.HOST}/callback`,  
+    'http://localhost:3000/callback' || `http://${process.env.HOST}:3000/callback`,  
   },
   ((accessToken, refreshToken, extraParams, profile, done) => {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
--- test/test.js
+++ test/test.js
@@ -1,4 +1,16 @@
-var assert = require('assert');
+const assert = require('assert');
+const db = require('../db/index.js');
+// console.log('fool', db.saveMessage)
+describe('Array', function() {
+  describe('#db.saveMessage()', function() {
+    it('should save one message to the database', function() {
+      let resultArray = [];
+      assert.equal(db.saveMessage('1', '1', 'This is text'), 1)
+    })
+  })
+})
+//create test to check for exist;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
