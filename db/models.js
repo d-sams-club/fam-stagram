82e84cda7481d3d3e9f692b6bc0547237f30c395
Darrian Brown <Darrian Brown>
2019-07-22 09:48:25 -0500

    process.env.port and changes

--- db/models.js
+++ db/models.js
@@ -11,7 +11,6 @@ const User = db.define('user', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: Sequelize.STRING },
   email: { type: Sequelize.STRING },
-  // password: { type: Sequelize.STRING },
   createdAt: {
     type: 'TIMESTAMP',
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
