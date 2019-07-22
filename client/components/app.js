65c316ae4c59cac81804edfdeb6fb1015e6b72c3
Semaj <semajbarthelemy55@gmail.com>
2019-07-21 23:14:46 -0500

    changed photos component to send get req to server and get photos

--- client/components/app.js
+++ client/components/app.js
@@ -13,7 +13,9 @@ const app = angular.module('app', ['ngRoute'])
         });
       };
       this.handleJoinFamClick = (code) => {
-        $http.post('/code', { code });
+        $http.post('/code', {
+          code
+        });
         console.log('join code: ', code);
       };
     },
@@ -90,11 +92,31 @@ const app = angular.module('app', ['ngRoute'])
   })
   .component('photos', {
     controller($http) {
+      this.photoLinks = [];
       this.reload = () => {
         setTimeout(() => {
           window.location.reload();
+          $http.get('/photos')
+            .then(({data}) => {
+              this.photos = photos;
+              console.log(photos);
+              photos.forEach((photo) => {
+                console.log(this);
+                this.photoLinks.push(`/photo/${photo.url}`);
+              });
+            });
         }, 0);
       };
+
+      $http.get('/photos')
+        .then(({data: photos}) => {
+          this.photos = photos;
+          console.log(photos);
+          photos.forEach((photo) => {
+            console.log(this);
+            this.photoLinks.push(`/photo/${photo.url}`);
+          });
+        });
       // httpService.getPictures()
     },
     templateUrl: 'templates/photos.html',
