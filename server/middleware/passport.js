e9a2f2b1cd0cf45cca95c870b57173dc45d55f21
Darrian <darrian.jordan.brown@gmail.com>
2019-07-22 09:50:35 -0500

    Revert "Server testing"

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -10,7 +10,7 @@ const strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-   `http://${process.env.HOST}:3000/callback`,  
+    `http://${process.env.HOST}:3000/user`,  
   },
   ((accessToken, refreshToken, extraParams, profile, done) => done(null, profile)
   ),
